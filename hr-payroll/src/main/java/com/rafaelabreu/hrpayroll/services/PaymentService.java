6968d63072435eeb8adb1a72836910fc7869f572
RafaTeron <rafael_sabreu@hotmail.com>
2023-08-28 20:24:57 -0300

    FeignClient

--- hr-payroll/src/main/java/com/rafaelabreu/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rafaelabreu/hrpayroll/services/PaymentService.java
@@ -10,21 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.rafaelabreu.hrpayroll.entities.Payment;
 import com.rafaelabreu.hrpayroll.entities.Worker;
+import com.rafaelabreu.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
-	@Autowired
-	private RestTemplate restTemplate;
-
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables); 
+	
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
