d67dee3365d55a5d80ca8c982f7d6ef28c31a00a
RafaTeron <rafael_sabreu@hotmail.com>
2023-09-09 19:06:51 -0300

    Resilience4j(CircuitBreaker)

--- hr-payroll/src/main/java/com/rafaelabreu/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/rafaelabreu/hrpayroll/resources/PaymentResource.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rafaelabreu.hrpayroll.entities.Payment;
 import com.rafaelabreu.hrpayroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
@@ -17,9 +19,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+    @CircuitBreaker(name = "paymentService", fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = paymentService.getPayment(workerId,days);
 		return ResponseEntity.ok(payment);
 	}
+    
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days, Throwable e) {
+        Payment payment = new Payment("Fallback", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
 }
